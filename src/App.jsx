78106bc9bc4d0aee8c0f43e1f59f850334890e51
Filipe <filipef.paulo@gmail.com>
2022-05-25 18:15:40 -0300

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import FormWidget from "./components/FormWidget";
+import "./styles/App.sass";
+
+function App() {
+  return (
+    <div className="main-container">
+      <FormWidget />
+    </div>
+  );
+}
+
+export default App;
